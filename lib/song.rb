f6d16d3489703ecc35a827ea99eb3dbe9f9b953e
Sarah Harrs <sarahharrs@gmail.com>
2019-07-13 23:17:47 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,23 +1,51 @@
 class Song 
   @@count = 0 
-  
+  @@genres = [ ]
+  @@arists = [ ]
+       
   def intitalize
+   @name = name
+   @artist = artist
     @@count += 1 
     @@genres << genre
-    @@artists << artists
+    @@artists << artist
   end 
   
    def self.count
      @@count 
    end
    
+   def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
+   
    def self.genres
-     @@genres = [ ]
+     @@genres
    end
    def self.artists
-     @@arists = [ ]
+     @@arists
    end
    
+   def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+   
   attr_accessor :genre :name :artist
-  attr_reader  :song
+  
 end
